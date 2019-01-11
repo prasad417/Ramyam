2bd74b9a0d1bce5bd7b683e9ad0198afe6350022
prasad <417.prasad@gmail.com>
2019-01-11 00:40:30 -0600

    Adding login and register screen

---
+++ Controller/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  Ramyam
+//
+//  Created by Venkata SPR Bathula on 1/11/19.
+//  Copyright © 2019 Prasad. All rights reserved.
+//
+
+import UIKit
+
+
+class RegisterViewController: UIViewController {
+    
+    //Textfields pre-linked with IBOutlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
