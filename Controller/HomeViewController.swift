c3ca172d646c2225b805c0f85974591e8310e36b
prasad <417.prasad@gmail.com>
2019-01-11 17:35:21 -0600

    Adding screen view

---
+++ Controller/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  Ramyam
+//
+//  Created by Venkata SPR Bathula on 1/11/19.
+//  Copyright © 2019 Prasad. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    //Textfields pre-linked with IBOutlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
